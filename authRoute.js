136a2c747cf0bf611f1934d7d7a56f2e5be4284e
venomVV <73206390+venomVV@users.noreply.github.com>
2021-06-25 04:47:00 +0100

    bbbbb

--- authRoute.js
+++ authRoute.js
@@ -1,5 +1,5 @@
-const User = require("./models/User");
-const Post = require("./models/Post");
+const User = require("./models/user");
+const Post = require("./models/post");
 const mongoose = require('mongoose');
 
 
